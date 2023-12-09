3eb105b7156dd773f9e70e4794330e286d5985af
Cat-JiYoung <kimyamae82@gmail.com>
2023-12-09 10:33:59 +0900

    [REFACTOR/#17] 중복되는 클래스 제거 / 함수화

--- app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/NoActionplanInsightActivity.kt
+++ app/src/main/java/com/growthook/aos/presentation/insight/noactionplan/NoActionplanInsightActivity.kt
@@ -1,28 +1,30 @@
 package com.growthook.aos.presentation.insight.noactionplan
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.growthook.aos.databinding.ActivityNoActionplanInsightBinding
+import com.growthook.aos.util.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
-class NoActionplanInsightActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityNoActionplanInsightBinding
+class NoActionplanInsightActivity :
+    BaseActivity<ActivityNoActionplanInsightBinding>({ ActivityNoActionplanInsightBinding.inflate(it) }) {
+    private lateinit var insightId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNoActionplanInsightBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        getIntentInsightId()
         setClickListeners()
-
-        val insightId = intent.getIntExtra("insightId", 0)
-        Timber.d("인사이트 id $insightId")
     }
 
     private fun setClickListeners() {
         clickMenu()
         clickBackBtn()
+    }
+
+    private fun getIntentInsightId() {
+        insightId = intent.getIntExtra("insightId", 0).toString()
+        Timber.d("인사이트 id $insightId")
     }
 
     private fun clickMenu() {
